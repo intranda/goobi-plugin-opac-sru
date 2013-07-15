7833d06927fb2cfbd3c1123971e51b1dd3cb9fc7
Robert Sehr <Robert.Sehr@intranda.com>
2013-07-15 14:15:57 +0200

    added ant script

--- SRUOpacPlugin/src/de/intranda/goobi/plugins/utils/SRUClient.java
+++ SRUOpacPlugin/src/de/intranda/goobi/plugins/utils/SRUClient.java
@@ -22,9 +22,10 @@ import de.intranda.utils.DocumentUtils;
 import de.unigoettingen.sub.search.opac.ConfigOpacCatalogue;
 
 public class SRUClient {
-    
+
     private static final Logger logger = Logger.getLogger(SruOpacImport.class);
     private static final String ENCODING = "UTF-8";
+
     /**
      * Queries the given catalog via Z.3950 (SRU) and returns its response.
      * 
@@ -35,17 +36,20 @@ public class SRUClient {
      */
     public static String querySRU(ConfigOpacCatalogue cat, String query, String recordSchema) {
         String ret = null;
+        if (query != null && !query.isEmpty()) {
+            query = query.trim();
+        }
 
         if (cat != null) {
             // ?version=1.1&operation=searchRetrieve&query=dinosaur&maximumRecords=5&recordSchema=marcxml
             String url = "http://";
-//            if ((cat.getLogin() != null) && (cat.getLogin().length() > 0)) {
-//                url += cat.getLogin();
-//                if ((cat.getPassword() != null) && (cat.getPassword().length() > 0)) {
-//                    url += ":" + cat.getPassword();
-//                }
-//                url += "@";
-//            }
+            //            if ((cat.getLogin() != null) && (cat.getLogin().length() > 0)) {
+            //                url += cat.getLogin();
+            //                if ((cat.getPassword() != null) && (cat.getPassword().length() > 0)) {
+            //                    url += ":" + cat.getPassword();
+            //                }
+            //                url += "@";
+            //            }
             url += cat.getAddress();
             url += ":" + cat.getPort();
             url += "/" + cat.getDatabase();
@@ -55,7 +59,7 @@ public class SRUClient {
             url += "&maximumRecords=5";
             url += "&recordSchema=" + recordSchema;
 
-             logger.debug("SRU URL: " + url);
+            logger.debug("SRU URL: " + url);
 
             HttpClient client = new HttpClient();
             GetMethod method = new GetMethod(url);
@@ -77,7 +81,7 @@ public class SRUClient {
 
         return ret;
     }
-    
+
     /**
      * Converts a <code>String</code> from one given encoding to the other.
      * 
@@ -101,38 +105,38 @@ public class SRUClient {
 
         return string;
     }
-    
+
     public static Document retrieveMarcRecord(String input) throws JDOMException, IOException {
         Document wholeDoc = DocumentUtils.getDocumentFromString(input, null);
         try {
-            Element record = wholeDoc.getRootElement().getChild("records", null).getChild("record", null).getChild("recordData", null).getChild("record", null);
-            Document outDoc = new Document((Element)record.detach());
+            Element record =
+                    wholeDoc.getRootElement().getChild("records", null).getChild("record", null).getChild("recordData", null)
+                            .getChild("record", null);
+            Document outDoc = new Document((Element) record.detach());
             return outDoc;
-        } catch(NullPointerException e) {
+        } catch (NullPointerException e) {
             throw new JDOMException("Unable to find marcxml record");
         }
     }
-    
+
     public static void main(String[] args) throws ConfigurationException, JDOMException, IOException {
-//        DataManager manager = DataManager.getInstance();
-//        manager.loadCatalogs(new File("resources/catalogs.xml").getAbsolutePath());
-//        Catalog cat = manager.getCatalogByName("HU Berlin Aleph");
-//        String query = "BV000388474";
-//        String recordSchema = cat.getFormatString();
-//        String answer = queryZ3950SRU(cat, query, recordSchema);
-////        System.out.println(answer);
-//        Document marcXmlDoc = retrieveMarcRecord(answer);
-////        System.out.println(DocumentUtils.getStringFromDocument(marcXmlDoc, null));
-//        File ruleset = new File("/opt/digiverso/goobi/rulesets/gdz.xml");
-//        File mapFile = new File("resources/marc_map.xml");
-//        try {
-//            Marc21Parser parser = new Marc21Parser(ruleset, mapFile);
-//            parser.parseMarcXml(marcXmlDoc);
-//        } catch (ParserException e) {
-//            logger.error(e);
-//        }
+        //        DataManager manager = DataManager.getInstance();
+        //        manager.loadCatalogs(new File("resources/catalogs.xml").getAbsolutePath());
+        //        Catalog cat = manager.getCatalogByName("HU Berlin Aleph");
+        //        String query = "BV000388474";
+        //        String recordSchema = cat.getFormatString();
+        //        String answer = queryZ3950SRU(cat, query, recordSchema);
+        ////        System.out.println(answer);
+        //        Document marcXmlDoc = retrieveMarcRecord(answer);
+        ////        System.out.println(DocumentUtils.getStringFromDocument(marcXmlDoc, null));
+        //        File ruleset = new File("/opt/digiverso/goobi/rulesets/gdz.xml");
+        //        File mapFile = new File("resources/marc_map.xml");
+        //        try {
+        //            Marc21Parser parser = new Marc21Parser(ruleset, mapFile);
+        //            parser.parseMarcXml(marcXmlDoc);
+        //        } catch (ParserException e) {
+        //            logger.error(e);
+        //        }
     }
-
-
 
 }
