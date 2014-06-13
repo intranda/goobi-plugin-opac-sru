84a94e10b9afd1a35c7082a7d6040cbfa583d0c5
Florian Alpers <florian.alpers@intranda.com>
2014-06-13 14:51:19 +0200

    fixed source header infomation

--- SRUOpacPlugin/src/de/intranda/goobi/plugins/utils/Marc21Parser.java
+++ SRUOpacPlugin/src/de/intranda/goobi/plugins/utils/Marc21Parser.java
@@ -1,22 +1,3 @@
-/**
- * This file is part of the SRU opac import plugin for the Goobi Application - a Workflow tool for the support of mass digitization.
- * 
- * Visit the websites for more information. 
- *          - http://digiverso.com 
- *          - http://www.intranda.com
- * 
- * Copyright 2013, intranda GmbH, Göttingen
- * 
- * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
- */
 
 package de.intranda.goobi.plugins.utils;
 
